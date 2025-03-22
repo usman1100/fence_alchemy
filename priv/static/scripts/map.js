531e690090899d27519d2ddda15e9ffc0d132919
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-03-22 22:37:23 +0500

    added polygon drawing

--- priv/static/scripts/map.js
+++ priv/static/scripts/map.js
@@ -1,4 +1,15 @@
-let socket = new window.PhoenixSocket("/socket", { params: { token: window.userToken } });
+let socket = new window.PhoenixSocket("/socket", {
+  params: { token: window.userToken },
+});
+
+const mapStyles = {
+  demo: "https://demotiles.maplibre.org/style.json",
+  arcgis:
+    "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json",
+  positron: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+  voyager: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json",
+  openStreet: 'https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json'
+};
 
 let markers = [];
 
@@ -8,7 +19,7 @@ const reDrawMarkers = (new_markers) => {
   });
 
   markers = new_markers.map((item) =>
-    new maplibregl.Marker().setLngLat([item.lng, item.lat]),
+    new maplibregl.Marker().setLngLat([item.lng, item.lat])
   );
 
   markers.forEach((marker) => {
@@ -32,7 +43,6 @@ channel
 channel.on("markers:new", (resp) => {
   reDrawMarkers(resp.markers);
 });
-
 
 const getRandomColor = () => {
   const colors = [
@@ -59,12 +69,38 @@ var m = maplibregl;
 
 var map = new m.Map({
   container: "map",
-  style: "https://demotiles.maplibre.org/style.json",
+  style: mapStyles.demo,
   center: [1.131057475965207, 8.556351936524663],
   zoom: 5,
 });
 
+MapboxDraw.constants.classes.CONTROL_BASE = "maplibregl-ctrl";
+MapboxDraw.constants.classes.CONTROL_PREFIX = "maplibregl-ctrl-";
+MapboxDraw.constants.classes.CONTROL_GROUP = "maplibregl-ctrl-group";
+
+const draw = new MapboxDraw({
+  displayControlsDefault: false,
+  controls: {
+    polygon: true, // Enable polygon drawing
+    trash: true, // Enable delete button
+  },
+});
+
+map.addControl(draw);
+
 map.on("click", (event) => {
+  const modes = draw.getMode();
+  if (
+    [
+      "draw_polygon",
+      "draw_line_string",
+      "direct_select",
+      "simple_select",
+    ].includes(modes)
+  ) {
+    return;
+  }
+
   const lat = event.lngLat.lat;
   const lng = event.lngLat.lng;
   channel.push("markers:new", {
@@ -73,11 +109,7 @@ map.on("click", (event) => {
   });
 });
 
-map.addControl(
-  new m.NavigationControl({
-    visualizePitch: true,
-    visualizeRoll: true,
-    showZoom: true,
-    showCompass: true,
-  }),
-);
+map.on("draw.create", (event) => {
+  console.log(event.features?.[0]);
+  console.log(event.features?.[0]?.geometry);
+});
