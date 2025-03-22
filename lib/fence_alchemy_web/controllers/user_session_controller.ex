398255d1315dc02d4a4ed40d7db3511fc590bf7a
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-03-22 21:38:21 +0500

    use import instead of alias

--- lib/fence_alchemy_web/controllers/user_session_controller.ex
+++ lib/fence_alchemy_web/controllers/user_session_controller.ex
@@ -1,8 +1,8 @@
 defmodule FenceAlchemyWeb.UserSessionController do
   use FenceAlchemyWeb, :controller
 
-  alias FenceAlchemy.Accounts
-  alias FenceAlchemyWeb.UserAuth
+  import FenceAlchemy.Accounts
+  import FenceAlchemyWeb.UserAuth
 
   def new(conn, _params) do
     render(conn, :new, error_message: nil)
@@ -11,10 +11,10 @@ defmodule FenceAlchemyWeb.UserSessionController do
   def create(conn, %{"user" => user_params}) do
     %{"email" => email, "password" => password} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
+    if user = get_user_by_email_and_password(email, password) do
       conn
       |> put_flash(:info, "Welcome back!")
-      |> UserAuth.log_in_user(user, user_params)
+      |> log_in_user(user, user_params)
     else
       # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
       render(conn, :new, error_message: "Invalid email or password")
@@ -24,6 +24,6 @@ defmodule FenceAlchemyWeb.UserSessionController do
   def delete(conn, _params) do
     conn
     |> put_flash(:info, "Logged out successfully.")
-    |> UserAuth.log_out_user()
+    |> log_out_user()
   end
 end
