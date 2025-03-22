3f17298bdaebbe8dc06d6e92699887fea2b82d91
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-03-23 00:53:14 +0500

    marker click events

--- priv/static/scripts/map.js
+++ priv/static/scripts/map.js
@@ -8,7 +8,8 @@ const mapStyles = {
     "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json",
   positron: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
   voyager: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json",
-  openStreet: 'https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json'
+  openStreet:
+    "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json",
 };
 
 let markers = [];
@@ -19,10 +20,14 @@ const reDrawMarkers = (new_markers) => {
   });
 
   markers = new_markers.map((item) =>
-    new maplibregl.Marker().setLngLat([item.lng, item.lat])
+    new maplibregl.Marker().setLngLat([item.lng, item.lat]),
   );
 
   markers.forEach((marker) => {
+    marker.getElement().addEventListener("click", (event) => {
+      event.stopPropagation();
+      console.log(event);
+    });
     marker.addTo(map);
   });
 };
@@ -44,32 +49,11 @@ channel.on("markers:new", (resp) => {
   reDrawMarkers(resp.markers);
 });
 
-const getRandomColor = () => {
-  const colors = [
-    "#FF6633",
-    "#FFB399",
-    "#FF33FF",
-    "#FFFF99",
-    "#00B3E6",
-    "#E6B333",
-    "#3366E6",
-    "#999966",
-    "#99FF99",
-    "#B34D4D",
-    "#80B300",
-    "#809900",
-    "#E6B3B3",
-    "#6680B3",
-    "#66991A",
-  ];
-  return colors[Math.floor(Math.random() * colors.length)];
-};
-
 var m = maplibregl;
 
 var map = new m.Map({
   container: "map",
-  style: mapStyles.demo,
+  style: mapStyles.arcgis,
   center: [1.131057475965207, 8.556351936524663],
   zoom: 5,
 });
@@ -90,14 +74,7 @@ map.addControl(draw);
 
 map.on("click", (event) => {
   const modes = draw.getMode();
-  if (
-    [
-      "draw_polygon",
-      "draw_line_string",
-      "direct_select",
-      "simple_select",
-    ].includes(modes)
-  ) {
+  if (["draw_polygon", "draw_line_string", "direct_select"].includes(modes)) {
     return;
   }
 
